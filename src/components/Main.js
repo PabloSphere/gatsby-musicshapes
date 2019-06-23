ab6c1740f8fe3daecd6dbeda5af04c40b918af16
Alan Thompson <pablosphere@gmail.com>
2019-06-23 14:26:00 -0400

    form final

--- src/components/Main.js
+++ src/components/Main.js
@@ -313,8 +313,7 @@ class Main extends React.Component {
 
           <p>
             Contact us to schedule a free 30-minute phone consultation, or send
-            us any questions you have about Music Therapy and our services.
-            Submit form below, or you can email us alanthompsonlcat@gmail.com{' '}
+            us any questions you have about Music Therapy and our services.{' '}
           </p>
 
           <form
